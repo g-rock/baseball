3562106e1dc0a8398cb7a5b95e3e5b271ac04887
Alex Giarrocco <giarroccoaj@gmail.com>
2020-08-29 13:34:48 -0400

    team logos; better stroke highlighting

--- src/components/GameMap.vue
+++ src/components/GameMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :id="team + 'GameMap'" />
+  <div :id="cleanTeamID" />
 </template>
 <script>
 /* eslint-disable */
@@ -17,13 +17,21 @@ export default {
       required: true
     }
   },
+  computed: {
+    cleanTeamID: function () {
+      let teamSplit = this.team.replace('.', '').split(' ')
+      return teamSplit.join('_')
+    }
+  },
   mounted () {
     // Configuration vars
     const X_TICKS = 10
     const CELL_SIZE = 40
-    const MARGIN = { top: 20, right: 20, bottom: 0, left: 0 }
+    const LOGO_SIZE = CELL_SIZE
+    const MARGIN = { top: LOGO_SIZE, right: 20, bottom: 0, left: 5 }
     var WIDTH = 750 - MARGIN.right - MARGIN.left
     var HEIGHT = 400 - MARGIN.top - MARGIN.bottom
+    var PADDING_INNER = 0.1 // padding between rects
 
     // should we calculate uniqueDates or uniqueGames?
     var uniqueDates = d3.set(this.games.map(function (d) { return d.game_date })).values()
@@ -31,12 +39,14 @@ export default {
 
     // --------SCALES-----------
     var xScale = d3.scaleBand()
-      .domain([...Array(X_TICKS).keys()])
       .range([0, X_TICKS * CELL_SIZE])
+      .domain([...Array(X_TICKS).keys()])
+      .paddingInner(PADDING_INNER * 2)
 
     var yScale = d3.scaleBand()
-      .domain([...Array(yAxisTicks).keys()])
       .range([0, yAxisTicks * CELL_SIZE])
+      .domain([...Array(yAxisTicks).keys()])
+      .paddingInner(PADDING_INNER)
 
     // --------AXIS-----------
     var xAxis = d3.axisTop().scale(xScale).tickSize(0)
@@ -48,16 +58,27 @@ export default {
       .attr('height', HEIGHT + MARGIN.top + MARGIN.bottom)
       .append('g')
 
-    svg.append('text')
-      .attr('class', 'title')
-      .attr('x', 0)
-      .attr('y', MARGIN.top)
-      .attr('font-family', 'sans-serif')
-      .attr('fill', colors[this.team].main)
-      .text(this.team)
+    // svg.append('text')
+    //   .attr('class', 'title')
+    //   .attr('x', 0)
+    //   .attr('y', MARGIN.top)
+    //   .attr('font-family', 'sans-serif')
+    //   .attr('fill', colors[this.team].main)
+    //   .text(this.team)
+
+    var imgs = svg.selectAll('img').data([0]);
+    imgs.enter()
+        .append('img:image')
+        .attr('xlink:href', require('@/assets/logos/' + this.team + '.gif'))
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', 60)
+        .attr('height', LOGO_SIZE)
+        .append('title')
+        .text(this.team)
 
     var gameGroup = svg.append('g')
-      .attr('transform', 'translate(' + MARGIN.left + ',' + MARGIN.top * 2 + ')')
+      .attr('transform', 'translate(' + MARGIN.left + ',' + (MARGIN.top + 10) + ')')
 
     // var xAxisGroup = gameGroup.append('g')
     //   .attr('class', 'axis')
@@ -65,7 +86,8 @@ export default {
 
     var yAxisGroup = gameGroup.append('g')
       .attr('class', 'axis')
-      .attr("transform", "translate(" + X_TICKS * CELL_SIZE + ",0)")
+      // PADDING_INNER should be a very tiny number, so it's ok to multiply by 100
+      .attr('transform', 'translate(' + (X_TICKS * CELL_SIZE + PADDING_INNER * 100) + ',0)')
       .call(yAxis)
 
     yAxisGroup.selectAll('text')
@@ -73,55 +95,70 @@ export default {
         return d3.select(this).text((d + 1) * 10)
       }) 
 
+    var Tooltip = d3.select('#' + this.cleanTeamID)
+      .append('div')
+      .style('opacity', 0)
+      .attr('class', 'tooltip')
+      .style('background-color', 'white')
+      .style('border', 'solid')
+      .style('border-width', '2px')
+      .style('border-radius', '5px')
+      .style('padding', '5px')
+
+    var mouseover = function(d) {
+      d3.select(this)
+        .attr('stroke-linejoin', 'round')
+        .attr('stroke-opacity', 1)
+        .attr('stroke-width', 2)
+        .style('stroke', 'black')
+    }
+    var mousemove = function(d) {
+      Tooltip
+        .html('The exact value of<br>this cell is: ' + d.value)
+        .style('left', (d3.mouse(this)[0]+70) + 'px')
+        .style('top', (d3.mouse(this)[1]) + 'px')
+    }
+    var mouseleave = function(d) {
+      d3.select(this)
+        .attr('stroke-linejoin', 'miter')
+        .attr('stroke-opacity', 0)
+        .attr('stroke-width', 1)
+        .style('stroke', 'white')
+    }
     // Can we loop over all grouped data (e.g: team games) and create rects for all of them ?
     gameGroup.selectAll('rect')
       .data(this.games)
-      .enter().append('g').append('rect')
-      .attr('class', 'cell')
-      .attr('width', CELL_SIZE)
-      .attr('height', CELL_SIZE)
-      .attr('y', function (d, i) {
-        if (i < X_TICKS) {
-          return yScale(0)
-        } else {
-          return yScale(i.toString()[0])
-        }
-      })
-      .attr('x', function (d, i) { return xScale(i % X_TICKS) })
-      .attr('fill', function (d, i) {
-        return colors[d.team_name].main
-      })
-      .attr('fill-opacity', function (d, i) {
-        if (d.win_status === 0) { return 0.5 } else { return 1.0 }
-      })
-      .attr('stroke', '#fff') 
-      .attr('stroke-width', 2)
-      // .attr('stroke-opacity', 0) 
-
-      
-    // svg.append('g')
-    //   .attr('class', 'y axis')
-    //   .call(yAxis)
-    //   .selectAll('text')
-    //   .attr('font-weight', 'normal')
-
-    // svg.append('g')
-    //   .attr('class', 'x axis')
-    //   .call(xAxis)
-    //   .selectAll('text')
-    //   .attr('font-weight', 'normal')
-    //   .style('text-anchor', 'start')
-    //   .attr('dx', '.8em')
-    //   .attr('dy', '.5em')
-    //   .attr('transform', function (d) {
-    //     return 'rotate(-65)'
-    //   })
+      .enter()
+      .append('g')
+      .append('rect')
+        .attr('class', 'cell')
+        .attr('width', CELL_SIZE)
+        .attr('height', CELL_SIZE)
+        .attr('y', function (d, i) {
+          if (i < X_TICKS) {
+            return yScale(0)
+          } else {
+            return yScale(i.toString()[0])
+          }
+        })
+        .attr('x', function (d, i) { return xScale(i % X_TICKS) })
+        .attr('margin-left', 2)
+        .attr('fill', function (d, i) {
+          return colors[d.team_name].main
+        })
+        .attr('fill-opacity', function (d, i) {
+          if (d.win_status === 0) { return 0.5 } else { return 0.9 }
+        })
+        .attr('stroke', 'white')
+        .attr('stroke-opacity', 0)
+      .on('mouseover', mouseover)
+      .on('mousemove', mousemove)
+      .on('mouseleave', mouseleave)
   }
 }
 </script>
 <style>
-  .axis path,
-  .axis line {
+  .axis path {
     stroke: white;
   }
 
